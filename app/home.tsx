96ac14b5f788303d51302bed7140597e144202c8
hadyamizari <117837424+hadya286@users.noreply.github.com>
2024-11-01 00:28:44 +0200

    Retrieving token using client-id and client-secret
    - axios post request to send id and secret to retrive the access token
    
    - logging the axios response of the token to check for success

--- app/home.tsx
+++ app/home.tsx
@@ -1,17 +1,31 @@
-import {SafeAreaView, StyleSheet, Text, TextInput, View} from 'react-native'
-import React, {useState} from 'react'
-import {MD3Theme, Searchbar, useTheme} from 'react-native-paper'
+import {SafeAreaView, StyleSheet, Text} from 'react-native'
+import React, {useEffect} from 'react'
+import {MD3Theme, useTheme} from 'react-native-paper'
 import {rem} from '@/constants/remUtils'
+import axios from 'axios'
+
+const getAccessToken = async () => {
+  const response = await axios.post('https://accounts.spotify.com/api/token', 'grant_type=client_credentials', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + btoa('9650f03f4d264da2b34a6444770e271a:0645010a3e2a46b291bd99e0ddd6d57e')
+    }
+  })
+  console.log('response token: ', response.data.access_token)
+  return response.data.access_token
+}
 
 const Home = () => {
   const theme = useTheme()
   const styles = createStyles(theme)
 
-  const [searchQuery, setSearchQuery] = useState('')
+  useEffect(() => {
+    getAccessToken()
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
-      <Searchbar placeholder='Search for an artist..' onChangeText={setSearchQuery} value={searchQuery} />
+      <Text>Home Screen</Text>
     </SafeAreaView>
   )
 }
